f11bfa2fea2296a9b6d31f7f082b32b0cb0fd347
mabreu88 <martymalkav7@gmail.com>
2023-11-17 16:32:44 -0800

    16. List shows and genres

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- A script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
