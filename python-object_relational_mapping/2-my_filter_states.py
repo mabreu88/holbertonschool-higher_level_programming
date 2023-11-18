ad5a9ce4702c50ebd385562907459bae5aedc165
mabreu88 <martymalkav7@gmail.com>
2023-11-18 10:07:48 -0800

    2. Filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a script that takes in an argument and displays all values in the\
+    states table of hbtn_0e_0_usa where name matches the argument. """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * from states WHERE name LIKE '{}' \
+              ORDER BY states.id".format(argv[4]))
+    for state in c.fetchall():
+        print(state)
+    c.close()
+    db.close()
