46106fdaec70e98ddd04ca35aa0c00345399d8e8
mabreu88 <martymalkav7@gmail.com>
2023-11-19 03:41:03 -0800

    10. Get a state

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""  a script that prints the State object with the name\
+      passed as argument from the database hbtn_0e_6_usa """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(
+        State.name == argv[4]).first()
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+    session.close()
