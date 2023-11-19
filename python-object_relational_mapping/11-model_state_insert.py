3c8341c941c709595e1b2c2d91929ef2367609a4
mabreu88 <martymalkav7@gmail.com>
2023-11-19 10:55:10 -0800

    11. Add a new state

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = State(name="Loiisiana")
+    state = State(name="Louisiana")
     session.add(state)
     session.commit()
     print(state.id)
